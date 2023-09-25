5c2928b2bc6c57e72c644c3df441eae3e40810a4
Didrik Sæther <didrik.saether@gmail.com>
2023-09-25 18:45:58 +0200

    Refactor in my heart

--- frontend/index.js
+++ frontend/index.js
@@ -12,53 +12,11 @@ function MerkelappGenerator() {
     const qrFieldId = "fldqHRKfnOo4TGxxR"
     const records = useRecords(table);
 
-    function createImageWithCanvas(record, canvasSize) {
-        return new Promise(async (resolve, reject) => {
-            const canvas = document.createElement('canvas');
-            const context = canvas.getContext('2d');
-            const img = new Image();
-            img.crossOrigin = 'anonymous';
-
-            // Set canvas size
-            canvas.width = canvasSize.width;
-            canvas.height = canvasSize.height;
-
-            // Fill the canvas with white color
-            context.fillStyle = 'white';
-            context.fillRect(0, 0, canvas.width, canvas.height);
-
-            img.src = record.getCellValue(qrFieldId)[0].url;
-
-            img.onload = () => {
-                // Draw the image on the canvas
-                context.drawImage(img, 0, 0, canvasSize.width, canvasSize.height);
-
-                // Add the Record Name text below the image
-                context.font = '8em Arial'; // Set font size and type
-                context.fillStyle = 'black'; // Set text color
-                context.textAlign = 'center';
-                context.fillText(record.name, canvas.width / 2, 665); // Adjust Y-coordinate as needed
-
-                // Convert the canvas to a blob
-                canvas.toBlob((blob) => {
-                    if (blob) {
-                        resolve(blob);
-                    } else {
-                        reject(`Error converting canvas to blob for ${record.name}`);
-                    }
-                }, 'image/png');
-            };
-
-            img.onerror = () => {
-                reject(`Error loading image for ${record.name}`);
-            };
-        });
-    }
-    const downloadPNG = (record) => {
+    function canvasGenerator() {
         const canvas = document.createElement('canvas');
         const context = canvas.getContext('2d');
         const img = new Image();
-        img.crossOrigin="anonymous"
+        img.crossOrigin = "anonymous"
 
         // Set canvas size to match the image dimensions
         canvas.width = 600;
@@ -66,15 +24,24 @@ function MerkelappGenerator() {
 
         context.fillStyle = 'white';
         context.fillRect(0, 0, canvas.width, canvas.height);
+        return {canvas, context, img};
+    }
+
+    function bildeOgTekstGenerator(context, img, record, canvas) {
+        context.drawImage(img, 0, 0, 600, 600);
+
+        // Add the Record Name text below the image
+        context.font = '8em Arial'; // Set font size and type
+        context.fillStyle = 'black'; // Set text color
+        context.textAlign = 'center';
+        context.fillText(record.name, canvas.width / 2, 665); // Adjust Y-coordinate as needed
+    }
+
+    const lastNedSiglePNG = (record) => {
+        const {canvas, context, img} = canvasGenerator();
 
         img.onload = () => {
-            context.drawImage(img, 0, 0, 600, 600);
-
-            // Add the Record Name text below the image
-            context.font = '8em Arial'; // Set font size and type
-            context.fillStyle = 'black'; // Set text color
-            context.textAlign = 'center';
-            context.fillText(record.name, canvas.width / 2, 665); // Adjust Y-coordinate as needed
+            bildeOgTekstGenerator(context, img, record, canvas);
 
             // Create a download link for the image
             const a = document.createElement('a');
@@ -86,35 +53,17 @@ function MerkelappGenerator() {
         img.src = record.getCellValue(qrFieldId)[0].url;
     };
 
-    const downloadZipFile = async () => {
+    const lastNedZipArkiv = async () => {
         const zip = new JSZip();
 
         const promises = records.map(async (record) => {
-            const canvas = document.createElement('canvas');
-            const context = canvas.getContext('2d');
-            const img = new Image();
-            img.crossOrigin="anonymous"
-
-            // Set canvas size to 2000x2000 pixels with a white background
-            canvas.width = 600;
-            canvas.height = 700;
-
-            // Fill the canvas with white color
-            context.fillStyle = 'white';
-            context.fillRect(0, 0, canvas.width, canvas.height);
+            const {canvas, context, img} = canvasGenerator();
 
             img.src = record.getCellValue(qrFieldId)[0].url;
 
             return new Promise((resolve, reject) => {
                 img.onload = () => {
-                    // Draw the image on the canvas
-                    context.drawImage(img, 0, 0, 600, 600);
-
-                    // Add the Record Name text below the image
-                    context.font = '8em Arial'; // Set font size and type
-                    context.fillStyle = 'black'; // Set text color
-                    context.textAlign = 'center';
-                    context.fillText(record.name, canvas.width / 2, 665); // Adjust Y-coordinate as needed
+                    bildeOgTekstGenerator(context, img, record, canvas);
 
                     // Convert the canvas to a blob
                     canvas.toBlob((blob) => {
@@ -135,10 +84,9 @@ function MerkelappGenerator() {
         });
 
         try {
-            await Promise.all(promises); // Wait for all canvas operations to complete
-            const content = await zip.generateAsync({ type: 'blob' });
+            await Promise.all(promises);
+            const content = await zip.generateAsync({type: 'blob'});
 
-            // Create a download link for the zip file
             const a = document.createElement('a');
             a.href = URL.createObjectURL(content);
             a.download = 'QRKoder.zip';
@@ -148,23 +96,22 @@ function MerkelappGenerator() {
         }
     };
 
-return (
-    <div>
-        <button onClick={downloadZipFile}>Last ned ZIP med alle QR-koder</button>
-        <ul style={{ listStyleType: 'none', padding: 0 }}>
-            {records.map((record) => (
-                <li key={record.id} style={{ listStyle: 'none' }}>
-                    <div style={{ textAlign: 'center' }}>
-                        <img src={record.getCellValue(qrFieldId)[0].url} alt="QR Kode for plantevegg" />
-                        <p style={{ fontSize: '4em', marginTop: '5px' }}>{record.name}</p> {}
-                        <button onClick={() => downloadPNG(record)}>Last ned kun denne QR-koden</button>
-                    </div>
-                </li>
-            ))}
-        </ul>
-    </div>
-);
+    return (
+        <div>
+            <button onClick={lastNedZipArkiv}>Last ned ZIP med alle QR-koder</button>
+            <ul style={{listStyleType: 'none', padding: 0}}>
+                {records.map((record) => (
+                    <li key={record.id} style={{listStyle: 'none'}}>
+                        <div style={{textAlign: 'center'}}>
+                            <img src={record.getCellValue(qrFieldId)[0].url} alt="QR Kode for plantevegg"/>
+                            <p style={{fontSize: '4em', marginTop: '5px'}}>{record.name}</p> {}
+                            <button onClick={() => lastNedSiglePNG(record)}>Last ned kun denne QR-koden</button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
-
 
 initializeBlock(() => <MerkelappGenerator/>);
